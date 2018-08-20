41b5214019dc832e6e54bd5acad02b340d697215
warpdesign <nicolas.ramz@gmail.com>
2018-08-20 17:02:26 +0200

    IMPROVED: now using custom JS node for mixing audio ADDED: realtime sampleSpeed change test with +/- keys

--- js/main.js
+++ js/main.js
@@ -8,12 +8,17 @@ async function betterFetch(url) {
     }
 }
 
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
 const ModPlayer = {
+    context: null,
+    mixerNode: null,
+
     async init(url) {
-        const buffer = await this.loadRawPCM(url);
-        this.prepareBuffer(buffer, {sampleRate:16000});
+        this.sampleData = await this.loadRawPCM(url);
+        this.createContext();
+        this.prepareBuffer(this.sampleData);
+        // this.createContext();
+        // this.prepareBuffer(buffer);
+        // this.prepareBuffer(buffer, {sampleRate:16000});
     },
 
     async loadRawPCM(url) {
@@ -24,44 +29,92 @@ const ModPlayer = {
         return buffer;
     },
 
-    prepareBuffer(buffer, { sampleSize = 2, sampleRate = 44100, channels = 2 } = {}) {
-        debugger;
-        // sampleSize = 2 bytes for 16bit sound
-        const frameCount = buffer.byteLength / sampleSize,
-            view = new Int16Array(buffer);
+    createContext({ bufferlen = 4096 } = {}) {
+        this.context = new (window.AudioContext || window.webkitAudioContext)();
 
-        this.audioBuffer = audioCtx.createBuffer(channels, frameCount, sampleRate);
+        this.outputRate = this.context.sampleRate;
 
-        for (let channel = 0; channel < channels; channel++) {
+        if (typeof this.context.createJavaScriptNode === 'function') {
+            this.mixerNode = this.context.createJavaScriptNode(bufferlen, 1, 1);
+        } else {
+            this.mixerNode = this.context.createScriptProcessor(bufferlen, 1, 1);
+        }
 
-            var nowBuffering = this.audioBuffer.getChannelData(channel);
-            for (var i = 0; i < frameCount; i++) {
-                // audio needs to be in [-1.0; 1.0]
-                // for this reason I also tried to divide it by 32767
-                // as my pcm sample is in 16-Bit. It plays still the
-                // same creepy sound less noisy.
-                var word = view[i];
-                // PCM data is signed, unsigned data would require a conversion: ((word + 32768) % 65536 - 32768)
-                nowBuffering[i] = word / 32768.0;
+        if (this.mixerNode) {
+            this.mixerNode.onaudioprocess = (ape) => this.mix(ape);
+        }
+    },
+
+    mix(audioProcessingEvent) {
+        var outputBuffer = audioProcessingEvent.outputBuffer.getChannelData(0);
+
+        for (var i = 0; i < outputBuffer.length; ++i) {
+            if (this.samplePos <= this.sampleLength - 1) {
+                outputBuffer[i] = this.buffer[Math.floor(this.samplePos)];
+            }
+            this.samplePos += this.sampleSpeed;
+            if (this.samplePos >= this.sampleLength - 1) {
+                this.samplePos = 0;
             }
         }
+    },
+
+    prepareBuffer(buffer, { sampleSize = 2, sampleRate = 44100, channels = 1 } = {}) {
+        // sampleSize = 2 bytes for 16bit sound
+        this.sampleLength = buffer.byteLength / sampleSize;
+
+        const view = new Int16Array(buffer);
+
+        // this.audioBuffer = audioCtx.createBuffer(channels, frameCount, sampleRate);
+
+        this.buffer = new Float32Array(this.sampleLength);
+        //this.audioBuffer.getChannelData(channel);
+        for (var i = 0; i < this.sampleLength; i++) {
+            // audio needs to be in [-1.0; 1.0]
+            var word = view[i];
+            // PCM data is signed, unsigned data would require a conversion: ((word + 32768) % 65536 - 32768)
+            this.buffer[i] = word / 32768.0;
+        }
+
+        this.samplePos = 0;
+        this.sampleSpeed = sampleRate / this.outputRate;
+
         console.log('audiobuffer ready');
+        console.log('outputRate', this.outputRate);
+        console.log('sampleSpeed=', this.sampleSpeed)
     },
 
     play() {
-        if (this.audioBuffer) {
-            // Get an AudioBufferSourceNode.
-            // This is the AudioNode to use when we want to play an AudioBuffer
-            var source = audioCtx.createBufferSource();
-            // set the buffer in the AudioBufferSourceNode
-            source.buffer = this.audioBuffer;
-            // connect the AudioBufferSourceNode to the
-            // destination so we can hear the sound
-            source.connect(audioCtx.destination);
-            // start the source playing
-            source.start();
-        }
+        console.log('playing');
+        this.mixerNode.connect(this.context.destination);
+        // if (this.audioBuffer) {
+        //     // Get an AudioBufferSourceNode.
+        //     // This is the AudioNode to use when we want to play an AudioBuffer
+        //     var source = audioCtx.createBufferSource();
+        //     // set the buffer in the AudioBufferSourceNode
+        //     source.buffer = this.audioBuffer;
+        //     source.playbackRate.value = 0.5;
+        //     // connect the AudioBufferSourceNode to the
+        //     // destination so we can hear the sound
+        //     source.connect(audioCtx.destination);
+        //     // start the source playing
+        //     source.start();
+        // }
+    },
+
+    stop() {
+        this.mixerNode.disconnect(this.context.destination);
     }
 }
 
-ModPlayer.init('audio/saxo_16k.raw');
+ModPlayer.init('audio/yahoo.raw');
+
+document.addEventListener('keyup', (e) => {
+    if (e.keyCode === 107) {
+        ModPlayer.sampleSpeed += 0.05;
+        console.log('new sampleSpeed', ModPlayer.sampleSpeed);
+    } else if (e.keyCode === 109) {
+        ModPlayer.sampleSpeed -= 0.05;
+        console.log('new sampleSpeed', ModPlayer.sampleSpeed);
+    }
+});
