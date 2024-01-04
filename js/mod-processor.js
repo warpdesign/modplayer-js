a6ff36dbdac403f70dc4f866fbdd50b78e8b4638
Nicolas Ramz <nicolas.ramz@adevinta.com>
2024-01-04 12:11:12 +0100

    modplayer: fixed merger input count

--- js/mod-processor.js
+++ js/mod-processor.js
@@ -404,9 +404,6 @@ class PTModuleProcessor extends AudioWorkletProcessor{
                 cmdData = data[3 + offset];
             const channel = this.channels[i];
 
-            // if (channel.id === 2 && this.row > 3)
-            //     debugger;
-
             channel.delay = 0;
 
             // check for command
@@ -833,10 +830,5 @@ const Effects = {
                 Module.calcTickSpeed();
             }
         }
-    },
-    0xFFF() {
-
     }
 }
-
-console.time('')
